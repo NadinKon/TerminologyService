6b184f234d65ed6bc373545dc09a16505f14e210
NadinKon <nadinkahf@gmail.com>
2024-07-21 12:07:52 +0300

    Add Dockerfile, docker-compose

---
+++ terminology_service/create_superuser.py
@@ -0,0 +1,19 @@
+import os
+import django
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "terminology_service.settings")
+django.setup()
+from django.contrib.auth.models import User
+
+# Получаем параметры суперпользователя из переменных окружения
+username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
+email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
+password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "admin")
+
+# Проверяем, существует ли пользователь с таким именем
+if username and email and password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username=username, email=email, password=password)
+else:
+    raise ValueError("Не установлены все необходимые переменные окружения для создания суперпользователя.")
--- terminology_service/terminology_service/settings.py
+++ terminology_service/terminology_service/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'refbooks.apps.RefbooksConfig',
     'rest_framework',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
